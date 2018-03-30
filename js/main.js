b550700de3e0f1958a3265c549ad24c1942ae355
ArtRudov <artrudov@gmail.com>
2018-03-30 15:17:02 +0300

    Небольшой рефакторинг кода

--- js/main.js
+++ js/main.js
@@ -1,52 +1,39 @@
-// Главный экран приложения
+// Главный экран приложения и основной Template
 const APP_SECTION = document.querySelector(`.app`);
-
-// Выборка шаблонов
 const TEMPLATE = document.querySelector(`template`);
-const WELCOME_SCREEN = TEMPLATE.content.querySelector(`.main--welcome`);
-const GENRE_SCREEN = TEMPLATE.content.querySelector(`.main--level-genre`);
-const ARTIST_SCREEN = TEMPLATE.content.querySelector(`.main--level-artist`);
-const RESULT_SCREEN = TEMPLATE.content.querySelectorAll(`.main--result`);
-
-// Массив экранов
-const GAME_SCREENS = [WELCOME_SCREEN, GENRE_SCREEN, ARTIST_SCREEN, RESULT_SCREEN[0], RESULT_SCREEN[1], RESULT_SCREEN[2]];
 
 // Клавиатурные коды
 const LEFT_KEY_CODE = 37;
 const RIGHT_KEY_CODE = 39;
 
-// Функция выводов экранов
-const renderScreen = () => {
-  const RENDER_TEMPLATE = GAME_SCREENS[counter].cloneNode(true);
-  const MAIN_SECTION = document.querySelector(`.main`);
+// Выборка шаблонов
+const welcomeScreen = TEMPLATE.content.querySelector(`.main--welcome`);
+const genreScreen = TEMPLATE.content.querySelector(`.main--level-genre`);
+const artistScreen = TEMPLATE.content.querySelector(`.main--level-artist`);
+const resultScreens = TEMPLATE.content.querySelectorAll(`.main--result`);
 
-  APP_SECTION.replaceChild(RENDER_TEMPLATE, MAIN_SECTION);
-};
-
-const getNextScreenNumber = (count, current) => {
-  const next = current % count;
-  return next + (next < 0 ? count : 0);
-};
+// Массив экранов
+const gameScreens = [welcomeScreen, genreScreen, artistScreen, resultScreens[0], resultScreens[1], resultScreens[2]];
 
 // Счетчик
 let counter = 0;
 
-// Функция отработки событий и смены экранов
-const CONTROL_KEY_HANDLER = (evt) => {
-    if (evt.altKey && LEFT_KEY_CODE) {
-      evt.preventDefault();
-      renderScreen();
-    }
-  //   case (evt.altKey && RIGHT_KEY_CODE):
-  //     evt.preventDefault();
-  //     if (counter === GAME_SCREENS.length - 1) {
-  //       break;
-  //     }
-  //     counter += 1;
-  //     renderScreen();
-  //     break;
-  // }
+// Функция выводов экранов
+const renderScreen = () => {
+  const renderTemplate = gameScreens[counter].cloneNode(true);
+  APP_SECTION.replaceChild(renderTemplate, APP_SECTION.children[0]);
 };
 
 renderScreen();
-document.addEventListener(`keydown`, CONTROL_KEY_HANDLER);
+document.addEventListener(`keydown`, (evt) => {
+  if (evt.keyCode === (evt.altKey && LEFT_KEY_CODE)) {
+    evt.preventDefault();
+    counter = (counter > 0) ? --counter : 0;
+    renderScreen();
+  } else if (evt.keyCode === (evt.altKey && RIGHT_KEY_CODE)) {
+    evt.preventDefault();
+    counter = (counter < gameScreens.length - 1) ? ++counter : gameScreens.length - 1;
+    renderScreen();
+  }
+});
+
